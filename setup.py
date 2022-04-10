0eef0db2c2a250830a4e1662901ee0ecf05bcba1
Niklas-AD <97914640+Niklas-AD@users.noreply.github.com>
2022-04-10 03:25:58 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -24,10 +24,10 @@ if 'CYTHONIZE_EVAL' in os.environ:
                            "evaluation", "addToConfusionMatrix.pyx")
     ext_modules = cythonize(pyxFile)
 
-with open("README.md", encoding="utf-8) as f:
+with open("README.md", encoding="utf-8") as f:
     readme = f.read()
 
-with open(os.path.join('cityscapesscripts', 'VERSION'), encoding="utf-8) as f:
+with open(os.path.join('cityscapesscripts', 'VERSION'), encoding="utf-8") as f:
     version = f.read().strip()
 
 console_scripts = [
